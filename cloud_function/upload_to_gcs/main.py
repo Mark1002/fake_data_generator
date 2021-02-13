1d3ff5ccf2a0ce8e5e1807782b58d5e2c2dbf0bd
Mark Chang <ak0922747401@gmail.com>
2021-02-13 19:34:19 +0800

    update avro for cloud function

--- cloud_function/upload_to_gcs/main.py
+++ cloud_function/upload_to_gcs/main.py
@@ -1,6 +1,5 @@
 """Cloud function for upload_fake_data_to_gcs."""
 import base64
-import json
 import traceback
 from google.cloud import storage
 
@@ -11,11 +10,10 @@ def upload_fake_data_to_gcs(event: dict, context):
         client = storage.Client()
         bucket_name = 'mark-etl'
         bucket = client.get_bucket(bucket_name)
-        doc = base64.b64decode(event['data']).decode('utf-8')
+        doc = base64.b64decode(event['data'])
         print(doc)
-        md5_id = json.loads(doc)['md5_id']
-        blob_name = f'test-stream/{md5_id}'
+        blob_name = f'test-stream/{context.event_id}'
         blob = bucket.blob(blob_name)
-        blob.upload_from_string(doc, content_type='application/json')
+        blob.upload_from_string(doc, content_type='avro/binary')
     except Exception:
         print(traceback.format_exc())
